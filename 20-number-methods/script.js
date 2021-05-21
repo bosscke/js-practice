e440b252bb2460ee5385be30eecd75aec0239ca3
bosscke <bosscke@yahoo.com>
2021-05-21 20:08:34 +0200

    Update number methods

--- 20-number-methods/script.js
+++ 20-number-methods/script.js
@@ -14,3 +14,21 @@ document.getElementById("test1").innerHTML =
   x.toExponential(4) +
   "<br>" +
   x.toExponential(6);
+
+//////////////////////////////////////////
+// toExponential()
+var x = 9.656;
+document.getElementById("test2").innerHTML =
+  x.toFixed(0) +
+  "<br>" +
+  x.toFixed(2) +
+  "<br>" +
+  x.toFixed(4) +
+  "<br>" +
+  x.toFixed(6);
+
+//////////////////////////////////////////
+// Number()
+
+var x = new Date("2020-05-21");
+document.getElementById("test3").innerHTML = Number(x);
