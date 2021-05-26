c131fbf48e778958f8147545dcce568fba45913b
bosscke <bosscke@yahoo.com>
2021-05-26 22:42:14 +0200

    Updating arrays

---
+++ 21-arrays/script.js
@@ -0,0 +1,12 @@
+////////////////////////////////////////
+// Access an array
+
+var cars = ["VW", "Skoda", "Dodge"];
+document.getElementById("test").innerHTML = cars[0];
+
+////////////////////////////////////////
+// Access an array
+
+var cars = ["VW", "Skoda", "Dodge"];
+cars[0] = "Porsche";
+document.getElementById("test1").innerHTML = cars[0];
