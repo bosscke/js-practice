05e3d2cde9be73ee1978c9ff51a5139e9115c3e7
bosscke <bosscke@yahoo.com>
2021-05-27 23:02:45 +0200

    Updating arrays

--- 21-arrays/script.js
+++ 21-arrays/script.js
@@ -10,3 +10,21 @@ document.getElementById("test").innerHTML = cars[0];
 var cars = ["VW", "Skoda", "Dodge"];
 cars[0] = "Porsche";
 document.getElementById("test1").innerHTML = cars[0];
+
+////////////////////////////////////////
+// Access the full Array
+
+var cars = ["Nissan", "Toyota", "Alfa Romeo"];
+document.getElementById("test2").innerHTML = cars;
+
+////////////////////////////////////////
+// Arrays are Objects
+
+var cars = ["Nissan", "Ferrari", "Ford"];
+document.getElementById("test3").innerHTML = cars[0];
+
+////////////////////////////////////////
+// Arrays are Objects
+
+var carSubName = { nissan: "Navara", ferrari: "Enzo", ford: "Mustang" };
+document.getElementById("test4").innerHTML = carSubName.nissan;
