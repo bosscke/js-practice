d487667e3a0689ad777c770a951802fad228c1d6
bosscke <bosscke@yahoo.com>
2021-06-06 23:19:05 +0200

    Updating array methods

--- 23-array-methods/script.js
+++ 23-array-methods/script.js
@@ -7,3 +7,11 @@ document.getElementById("test").innerHTML = fruits.toString();
 //// join()
 var fruits = ["Banana", "Orange", "Apple", "Mango"];
 document.getElementById("test1").innerHTML = fruits.join(" * ");
+
+//////////////////////////////////////////////////////////
+//// pop()
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("test2").innerHTML = fruits;
+document.getElementById("test3").innerHTML = fruits.pop();
+document.getElementById("test4").innerHTML = fruits;
