73b506b60dfd799d43fbbac287033b251decab4c
bosscke <bosscke@yahoo.com>
2021-06-08 20:53:28 +0200

    Adding 0-projecs directory

---
+++ 0-projects/1-project/main.js
@@ -0,0 +1,26 @@
+(function (window) {
+  // Game
+  var Game = function (el, option) {};
+  // Info section
+  // Deck
+  // Discard Pile
+  // Rules
+
+  // Deck
+  // Cards
+  // ------
+  // shuffle
+  // stack
+
+  // Cards
+  // val
+  // suit
+  // ------
+  // flip
+
+  // Discard Pile
+  // Holders
+  // ------
+  // accept or reject
+  window.Game = Game;
+})(window);
