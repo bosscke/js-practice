ee91db0323c7e273481687d998a6782162d801b5
bosscke <bosscke@yahoo.com>
2021-06-10 23:26:55 +0200

    Adding processes

---
+++ 0-projects/4-project/processes.js
@@ -0,0 +1,23 @@
+// ITIL_v3 processes with actvities
+
+const itil_v3 = {
+  lifecycle_phase: {
+    strategy: {
+      process_1: {
+        strategy: {
+          activity_group: {
+            strategic_assesment: {
+              activity_1a: "analyse internal factors",
+              activity_1b: "analyse external factors",
+              activity_2a: "define market spaces",
+              activity_2b: "identify strategic industry factors",
+              activity_3: "establish objectives",
+            },
+            strategy_generation: {},
+            strategy_execution: {},
+          },
+        },
+      },
+    },
+  },
+};
