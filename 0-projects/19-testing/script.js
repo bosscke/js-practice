148a7302893d03430d4183b2e2578ead0ec354f0
bosscke <bosscke@yahoo.com>
2021-07-12 22:31:52 +0200

    Update

--- 0-projects/19-testing/script.js
+++ 0-projects/19-testing/script.js
@@ -7,11 +7,47 @@ function updateMessage() {
 }
 updateMessage();
 
+// Declaring a Functions
+function sayHello() {
+  document.write("Hello!");
+}
+sayHello(); // Calling the function
+
+// Declaring the function
+function sayHi() {
+  document.write("<br>" + "Hi!");
+}
+sayHi(); // Calling the function
+
+// Declaring Functions that need information
 function getArea(width, height) {
   return width * height;
 }
 
+// Calling Functions that need informations
 wallWidth = 3;
 wallHeight = 5;
 getArea(3, 5);
 getArea(wallWidth, wallHeight);
+
+// Getting a single value out of a function
+
+function calculateArea(width, height) {
+  let area = width * height;
+  return area;
+}
+
+let wallOne = calculateArea(3, 5);
+let wallTwo = console.log(calculateArea(8, 5));
+
+// Getting multiple values out of a function
+
+function getSize(width, height, depth) {
+  let area = width * height;
+  let volume = width * height * depth;
+  let sizes = [area, volume];
+  return sizes;
+}
+
+let areaOne = console.log(getSize(3, 2, 3)[0]);
+let volumeOne = console.log(getSize(3, 2, 3)[1]);
