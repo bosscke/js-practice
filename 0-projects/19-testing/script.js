ea16cba16bcdb741a2778020c96c6aea6b83b9e9
bosscke <bosscke@yahoo.com>
2021-07-15 23:29:07 +0200

    Update

--- 0-projects/19-testing/script.js
+++ 0-projects/19-testing/script.js
@@ -1,5 +1,5 @@
 // Objects
-
+//Literal Notation
 let hotel = {
   name: "Las Vegas",
   rooms: 100,
@@ -16,4 +16,21 @@ console.log(hotelName);
 let roomsFree = hotel.checkAvailability();
 console.log(roomsFree);
 
-//
+// Object
+// Constructor Notation
+
+let hotel1 = new Object();
+
+hotel1.name = "Babba";
+hotel1.rooms = 25;
+hotel1.booked = 11;
+
+hotel1.checkAvailability = function () {
+  return this.rooms - this.booked;
+};
+
+console.log(hotel1.name);
+delete hotel1.name;
+console.log(hotel1.name);
+hotel1.name = "Starra";
+console.log(hotel1.name);
