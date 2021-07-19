0c585d9eed8fca9996b83e194f9d6439a695df39
bosscke <bosscke@yahoo.com>
2021-07-19 23:23:18 +0200

    Update

---
+++ 0-projects/20-js-the-definitive-guide/script.js
@@ -0,0 +1,15 @@
+console.log(0b10101);
+console.log(0xbadcafe);
+console.log(0xff);
+console.log(0o377);
+let billion = 1_000_000_000;
+console.log(billion);
+
+// Obtaining portion of a string
+let text = "some text here";
+console.log(text.substring(2, 4));
+console.log(text.slice(-2));
+console.log(text.split(","));
+
+//Searching a string
+console.log(text.indexOf("t"));
