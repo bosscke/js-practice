0b609a061fb772662fef4749a4c22118b9d1f6d5
bosscke <bosscke@yahoo.com>
2021-07-28 23:01:53 +0200

    Update

--- 0-projects/20-js-the-definitive-guide/script.js
+++ 0-projects/20-js-the-definitive-guide/script.js
@@ -66,4 +66,17 @@ ${exception.stack}
 `;
 
 console.log(errorMessage);
-//
+
+// Symbols
+
+let strname = "string name";
+let symname = Symbol("propname");
+
+console.log(typeof strname);
+console.log(typeof symname);
+
+let o = {};
+o[strname] = 1;
+o[symname] = 2;
+console.log(o[strname]);
+console.log(o[symname]);
